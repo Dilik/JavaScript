e467d3d0780d5dd4747014aad4b8a9810fb9c233
@Dilmurod_Makhamadaliev <1989Deny@gmail.com>
2020-11-03 16:34:47 -0900

    functions

---
+++ FullStack Web Development Boocamp/4. Javascript/4 Functions/functionMethod.js
@@ -0,0 +1,12 @@
+const mathFunc = {
+    PI: 3.14159,
+    square: function(num){
+        return num*num;
+    },
+    cube(num){
+        return num**3;
+    },
+    add(num1, num2){
+        return num1+num2;
+    }
+}
