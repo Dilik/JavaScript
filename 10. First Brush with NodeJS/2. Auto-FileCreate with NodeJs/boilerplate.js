09a375dade29f6688bebfc078ec2b15a5fb11f0f
@Dilmurod_Makhamadaliev <1989Deny@gmail.com>
2020-11-15 18:34:45 -0900

    Intro to Node

---
+++ 10. First Brush with NodeJS/2. Auto-FileCreate with NodeJs/boilerplate.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const folderName = process.argv[2] || 'Project'
+fs.mkdirSync(folderName);
+fs.writeFileSync(`${folderName}/index.html`)
+fs.writeFileSync(`${folderName}/style.css`)
+fs.writeFileSync(`${folderName}/app.js`)
