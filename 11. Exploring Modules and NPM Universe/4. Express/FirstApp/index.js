c386ac324477f2decd2868cfd6663e52803e3619
@Dilmurod_Makhamadaliev <1989Deny@gmail.com>
2020-11-20 18:40:57 -0900

    Express route and params

--- 11. Exploring Modules and NPM Universe/4. Express/FirstApp/index.js
+++ 11. Exploring Modules and NPM Universe/4. Express/FirstApp/index.js
@@ -1,8 +1,53 @@
 const express = require("express");
 const app = express();
 
-app.use(()=> {
-    console.log("We Got A New Request");
+// app.use((req, res)=> {
+//     console.log("We Got A New Request");
+//     res.send('<h1>Welocme to Express !!!</h1>');   
+// })
+
+//routing
+app.get('/', (req, res)=>{
+    res.send('<h1>Welcome to Express, Nodemon is Monitoring the Port</h1>'); 
+})
+
+app.get('/dogs', (req, res)=>{
+    res.send('<h1>Woof</h1>'); 
+    console.log('woof');
+})
+
+app.post('/dogs', (req,res)=>{
+    res.send("this is a POST request to dogs");
+})
+
+app.get('/cats', (req, res)=>{
+    res.send('<h1>Meow</h1>')
+})
+
+//subredit - sub parameter passed to req.
+app.get('/r/:subreddit', (req, res)=>{
+    const {subreddit} = req.params
+    res.send(`Welcome to the ${subreddit} subreddit`); 
+})
+
+//creating multiple subreddit path
+app.get('/r/:subreddit/:postId', (req, res)=>{
+    const {subreddit, postId} = req.params
+    res.send(`Welcome to the post ID ${postId} udner ${subreddit} subreddit`); 
+})
+
+//search query
+app.get('/search', (req, res)=>{
+    const {q} = req.query;
+    if(!q){
+        res.send('Nothing found if nothing searched');
+    }
+    res.send(`Search result for ${q}`);
+})
+
+//use it at the end. if it is at the beginning it will disregard all
+app.get('*', (req,res)=>{
+    res.send("Error 404, Page Do Not Exist!")
 })
 
 app.listen(3000, ()=>{
