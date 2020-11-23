60f3fa272e7e0927b2e77efb7093de6970d08e47
@Dilmurod_Makhamadaliev <1989Deny@gmail.com>
2020-11-22 20:39:55 -0900

    templating

---
+++ 12. Creating Dynamic HTML with Templating/2. Serving Static Assets in Express/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const reditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+app.get('/rand', (req, res)=>{
+    const random = Math.floor(Math.random()*10)+1;
+    res.render('random',{random})
+})
+
+app.get('/cat', (req, res)=>{
+    const cats = ['Alli', 'jimmy', 'carter', 'smokey'];
+    res.render('cat', { cats });
+})
+
+app.get('/r/:subreddit', (req, res)=>{
+    const {subreddit} = req.params;
+    const data = reditData[subreddit]; 
+    if(data){
+        res.render('subreddit', {...data})
+    } else {
+        res.render('notfound', {subreddit})
+    }
+})
+
+app.listen('3000', ()=>{
+    console.log('Listening Port 3000');
+})
